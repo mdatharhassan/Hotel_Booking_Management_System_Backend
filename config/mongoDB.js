7f134d65368b6e6088de231f5ada261e2e037a45
athar <iitropar109@gmail.com>
2025-12-09 21:47:40 +0530

    Initial commit

---
+++ config/mongoDB.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const connectDB = async (mongoURL) => {
+  try {
+    await mongoose.connect(mongoURL);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
+
+export default connectDB;
+
+// mongoose.connect("mongoURL").then(() => {
+//   console.log("MongoDB connected successfully");
+// }).catch((error) => {
+//   console.error("MongoDB connection error:", error.message);
+// });
+// mongoose.connection.on("connected", () => {
+//   console.log("Mongoose connected to DB");
+// });
+// mongoose.connection.on("error", (err) => {
+//   console.log("Mongoose connection error:", err);
+// });
+// mongoose.connection.on("disconnected", () => {
+//   console.log("Mongoose disconnected from DB");
+// });
---
+++ server.js
@@ -0,0 +1,67 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.js";
+import cabinRouter from "./routes/cabin.js";
+import bookingRouter from "./routes/booking.js";
+import connectDB from "./config/mongoDB.js";
+import settingRouter from "./routes/settings.js";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+// Cors configuration
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  method: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+};
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.send("Innsight Admin Server is running");
+});
+
+// Middleware to parse JSON requests
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// API Routes
+app.use("/api/auth", authRouter);
+app.use("/api/cabins", cabinRouter);
+// app.use("api/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api/bookings", bookingRouter);
+app.use("/api/settings", settingRouter);
+
+// Connect to MongoDB
+connectDB(MONGO_URL);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
