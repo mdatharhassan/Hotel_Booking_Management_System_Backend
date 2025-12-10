76b10db89a4908313be9075d01035f8898332878
athar <iitropar109@gmail.com>
2025-12-10 18:30:11 +0530

    finalfixednew

--- api/server.js
+++ api/server.js
@@ -22,8 +22,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URL;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 app.get("/favicon.ico", (req, res) => {
   res.status(204).end();
 });
@@ -37,21 +37,21 @@ const corsOptions = {
 
 // Middleware to parse JSON requests
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(morgan("dev"));
+// app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Innsight Admin Server is running");
 });
 // API Routes
-app.use("/api/auth", authRouter);
-app.use("/api/cabins", cabinRouter);
-// app.use("api/uploads", express.static("uploads"));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/api/bookings", bookingRouter);
-app.use("/api/settings", settingRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/cabins", cabinRouter);
+// // app.use("api/uploads", express.static("uploads"));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/api/bookings", bookingRouter);
+// app.use("/api/settings", settingRouter);
 
 // this is for localhost
 // connectDB(MONGO_URL)
