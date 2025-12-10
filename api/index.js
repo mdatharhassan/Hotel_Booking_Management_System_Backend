eeb22607ec7e86d0ef6041399324d292651a334f
athar <iitropar109@gmail.com>
2025-12-10 20:06:40 +0530

    finalfixednew

--- api/index.js
+++ api/index.js
@@ -1,29 +1,29 @@
 import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
-// import morgan from "morgan";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// import cookieParser from "cookie-parser";
-// import authRouter from "../routes/auth.js";
-// import cabinRouter from "../routes/cabin.js";
-// import bookingRouter from "../routes/booking.js";
-// import connectDB from "../config/mongoDB.js";
-// import settingRouter from "../routes/settings.js";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import authRouter from "../routes/auth.js";
+import cabinRouter from "../routes/cabin.js";
+import bookingRouter from "../routes/booking.js";
+import connectDB from "../config/mongoDB.js";
+import settingRouter from "../routes/settings.js";
 import module from "module";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import serverless from "serverless-http";
 
 // Load environment variables from .env file
-// dotenv.config();
+dotenv.config();
 
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 // const MONGO_URI = process.env.MONGO_URL;
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // app.get("/favicon.ico", (req, res) => {
 //   res.status(204).end();
 // });
@@ -38,26 +38,26 @@ const corsOptions = {
 // Middleware to parse JSON requests
 app.use(cors(corsOptions));
 app.use(express.json());
-// app.use(morgan("dev"));
-// app.use(cookieParser());
-// app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Innsight Admin Server is running");
 });
 // API Routes
-// app.use("/api/auth", authRouter);
-// app.use("/api/cabins", cabinRouter);
-// // app.use("api/uploads", express.static("uploads"));
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-// app.use("/api/bookings", bookingRouter);
-// app.use("/api/settings", settingRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/cabins", cabinRouter);
+// app.use("api/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/bookings", bookingRouter);
+app.use("/api/settings", settingRouter);
 
 // this is for localhost
-// connectDB(process.env.MONGO_URL);
-// app.listen(PORT, () => {
-//   console.log(`Server running locally on port ${PORT}`);
-// });
+connectDB(process.env.MONGO_URL);
+app.listen(PORT, () => {
+  console.log(`Server running locally on port ${PORT}`);
+});
 
 //this is for vercel
 // Middleware that ensures DB is connected before handling request
