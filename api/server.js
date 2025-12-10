74cae24d09b4d9954a7c2b7c9bc2cb164c137b01
athar <iitropar109@gmail.com>
2025-12-10 16:17:16 +0530

    atharfixed

--- api/server.js
+++ api/server.js
@@ -19,34 +19,22 @@ import serverless from "serverless-http";
 
 // Initialize Express app
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Innsight Admin Server is running");
-});
-//const PORT = process.env.PORT || 5000;
-//const MONGO_URI = process.env.MONGO_URL;
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URL;
 
 // Cors configuration
 const corsOptions = {
-  origin: "https://hotel-booking-management-system-fro.vercel.app/",
+  origin: ["https://hotel-booking-management-system-fro.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   next();
-// });
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// app.get("/", (req, res) => {
-//   res.send("Innsight Admin Server is running");
-// });
+app.get("/", (req, res) => {
+  res.send("Innsight Admin Server is running");
+});
 
 // Middleware to parse JSON requests
 app.use(cors(corsOptions));
@@ -54,19 +42,12 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-// app.use((req, res, next) => {
-//   if (!isConnected) {
-//     connecttoMongoDB();
-//   }
-//   next();
-// });
 
 // API Routes
 app.use("/api/auth", authRouter);
 app.use("/api/cabins", cabinRouter);
 // app.use("api/uploads", express.static("uploads"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 app.use("/api/bookings", bookingRouter);
 app.use("/api/settings", settingRouter);
 
@@ -84,7 +65,7 @@ app.use("/api/settings", settingRouter);
 //   await connectDB(MONGO_URI);
 //   next();
 // });
-
+connectDB(MONGO_URI);
 // // Start the server
 // Start server when running locally (avoid starting listener in production/serverless)
 if (process.env.NODE_ENV !== "production") {
